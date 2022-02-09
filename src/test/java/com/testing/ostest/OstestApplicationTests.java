ee107b2915ee3ef8b3bdfbab1b175eb99b2f9408
joe <skchan222@gmail.com>
2022-02-09 22:35:46 +0800

    project initial

---
+++ src/test/java/com/testing/ostest/OstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.ostest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
